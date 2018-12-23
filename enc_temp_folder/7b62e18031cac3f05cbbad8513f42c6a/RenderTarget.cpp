18841b0b779e5a5f8620fde9cce9983ffc374f37
Fingal <akokosza@o2.pl>
2018-12-23 19:43:50 +0100

    useless lines of code removed

--- enc_temp_folder/7b62e18031cac3f05cbbad8513f42c6a/RenderTarget.cpp
+++ enc_temp_folder/7b62e18031cac3f05cbbad8513f42c6a/RenderTarget.cpp
@@ -10,6 +10,39 @@ RenderTarget::RenderTarget()
 {
 }
 
+RenderTarget::RenderTarget(obj::Model& model, GLuint& program) {
+	glGenVertexArrays(1, &VertexArray);
+	glBindVertexArray(VertexArray);
+	glGenBuffers(1, &VertexIndexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VertexIndexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, model.faces["default"].size() * sizeof(unsigned short), &model.faces["default"][0], GL_STATIC_DRAW);
+
+
+	glGenBuffers(1, &VertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+	int buffer_size = model.vertex.size() * sizeof(float) + model.normal.size() * sizeof(float) + model.texCoord.size() * sizeof(glm::vec2);
+	glBufferData(GL_ARRAY_BUFFER, buffer_size, NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, model.vertex.size() * sizeof(float), &model.vertex[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, model.vertex.size() * sizeof(float), model.normal.size() * sizeof(float), &model.normal[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, model.vertex.size() * sizeof(float) + model.normal.size() * sizeof(float), model.texCoord.size() * sizeof(glm::vec2), &model.texCoord[0]);
+
+
+	this->program = program;
+	glPos = glGetAttribLocation(program, "vertexPosition");
+	glEnableVertexAttribArray(glPos);
+	glVertexAttribPointer(glPos, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+	glNor = glGetAttribLocation(program, "vertexNormal");
+	glEnableVertexAttribArray(glNor);
+	glVertexAttribPointer(glNor, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(model.vertex.size() * sizeof(float)));
+
+	glUV = glGetAttribLocation(program, "vertexTexCoord");
+	glEnableVertexAttribArray(glUV);
+	glVertexAttribPointer(glUV, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(model.vertex.size() * sizeof(float) + model.normal.size() * sizeof(float)));
+	glBindVertexArray(0);
+	size = model.faces["default"].size();
+}
+
+
 void RenderTarget::loadModelFromFile(const char* fileName) {
 	obj::Model model = obj::loadModelFromFile(fileName);
 	glGenVertexArrays(1, &VertexArray);
--- src/main.cpp
+++ src/main.cpp
@@ -19,26 +19,9 @@
 
 RenderMaterial sphere;
 
-GLuint programColor;
-GLuint programDepth;
 GLuint programTexture;
-GLuint programRender;
-
-GLuint depthTexture;
-GLuint FrameBufferObject;
-
-
-GLuint VertexArrayID;
-GLuint VertexBuffer;
-GLuint VertexIndexBuffer;
 
 Core::Shader_Loader shaderLoader;
-
-obj::Model shipModel;
-obj::Model sphereModel;
-obj::Model renderModel;
-
-//model sphereModel;
 
 glm::vec3 cameraPos = glm::vec3(0, 0, 3);
 glm::vec3 cameraDir;
@@ -51,11 +34,6 @@ glm::vec3 lightDir = glm::normalize(glm::vec3(1.0f, -0.9f, -1.0f));
 glm::quat rotation = glm::quat(1, 0, 0, 0);
 glm::vec3 rotationChangeXYZ = glm::vec3(0, 0, 0);
 
-GLuint textureAsteroid;
-GLuint textureEarth;
-GLuint textureMetallic;
-GLuint textureRoughness;
-GLuint textureAlbedo;
 
 GLuint cubemapTexture;
 
@@ -105,80 +83,6 @@ glm::mat4 createCameraMatrix()
 
 	return Core::createViewMatrixQuat(cameraPos, rotation);
 }
-
-
-
-void drawObjectColor(obj::Model * model, glm::mat4 modelMatrix, glm::vec3 color)
-{
-	GLuint program = programColor;
-
-	glUseProgram(program);
-
-
-	glUniform3f(glGetUniformLocation(program, "lightDir"), lightDir.x, lightDir.y, lightDir.z);
-	glUniform3f(glGetUniformLocation(program, "cameraPos"), cameraPos.x, cameraPos.y, cameraPos.z);
-
-	glm::mat4 transformation = perspectiveMatrix * cameraMatrix * modelMatrix;
-	glUniformMatrix4fv(glGetUniformLocation(program, "modelViewProjectionMatrix"), 1, GL_FALSE, (float*)&transformation);
-	glUniformMatrix4fv(glGetUniformLocation(program, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
-
-	
-	Core::DrawModel(model);
-
-
-	glUseProgram(0);
-}
-
-
-void drawObjectTexture(obj::Model * model, glm::mat4 modelMatrix, GLuint textureId)
-{
-	GLuint program = programTexture;
-
-	glUseProgram(program);
-	//float metallic = 0.5f;
-	//GLint metallicLoc = glGetUniformLocation(program, "metallic");
-	//float roughness = 0.5f;
-	//GLint roughnessLoc = glGetUniformLocation(programTexture, "roughness");
-	//glUniform1f(roughnessLoc, roughness);
-
-	glUniform3f(glGetUniformLocation(program, "lightDir"), lightDir.x, lightDir.y, lightDir.z);
-	glUniform3f(glGetUniformLocation(program, "cameraPos"), cameraPos.x, cameraPos.y, cameraPos.z);
-	Core::SetActiveTexture(textureId, "Tcolor", program, 0);
-	Core::SetActiveTexture(textureMetallic, "Tmetallic", program, 1); 
-	Core::SetActiveTexture(textureRoughness, "Troughness", program, 2);
-	Core::SetActiveTexture(textureAlbedo, "Talbedo", program, 3);
-
-	glm::mat4 transformation = perspectiveMatrix * cameraMatrix * modelMatrix;
-	glUniformMatrix4fv(glGetUniformLocation(program, "modelViewProjectionMatrix"), 1, GL_FALSE, (float*)&transformation);
-	glUniformMatrix4fv(glGetUniformLocation(program, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
-
-	Core::DrawModel(model);
-
-	glUseProgram(0);
-}
-//void drawVAOTexture(RenderTarget& target) {
-//	glUseProgram(programTexture);
-//	glBindVertexArray(target.VertexArray);
-//	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VertexIndexBuffer);
-//	glUniform3f(glGetUniformLocation(programTexture, "lightDir"), lightDir.x, lightDir.y, lightDir.z);
-//	glUniform3f(glGetUniformLocation(programTexture, "cameraPos"), cameraPos.x, cameraPos.y, cameraPos.z);
-//	Core::SetActiveTexture(textureEarth, "Tcolor", programTexture, 0);
-//	Core::SetActiveTexture(textureMetallic, "Tmetallic", programTexture, 1);
-//	Core::SetActiveTexture(textureRoughness, "Troughness", programTexture, 2);
-//	Core::SetActiveTexture(textureAlbedo, "Talbedo", programTexture, 3);
-//	glm::mat4 modelMatrix(1.0f);
-//	glm::mat4 transformation = perspectiveMatrix * cameraMatrix * modelMatrix;
-//	glUniformMatrix4fv(glGetUniformLocation(programTexture, "modelViewProjectionMatrix"), 1, GL_FALSE, (float*)&transformation);
-//	glUniformMatrix4fv(glGetUniformLocation(programTexture, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
-//	glDrawElements(
-//		GL_TRIANGLES,      // mode
-//		target.size,    // count
-//		GL_UNSIGNED_SHORT,   // type
-//		(void*)0           // element array buffer offset
-//	);
-//	glBindVertexArray(0);
-//	glUseProgram(0);
-//}
 void renderScene()
 {
 	
@@ -203,51 +107,16 @@ void init()
 {
 	srand(time(0));
 	glEnable(GL_DEPTH_TEST);
-	programColor = shaderLoader.CreateProgram("shaders/shader_color.vert", "shaders/shader_color.frag");
 	programTexture = shaderLoader.CreateProgram("shaders/shader_tex.vert", "shaders/shader_tex.frag");
-	sphereModel = obj::loadModelFromFile("models/sphere.obj");
 	
 	sphere = RenderMaterial();
 	sphere.loadModelFromFile("models/sphere.obj");
 	sphere.setProgram(programTexture);
 	sphere.loadMaterial("textures/rustediron2_roughness.png", "textures/rustediron2_metallic.png", "textures/rustediron2_basecolor.png");
-
-
-
-
-	/*glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-	glGenBuffers(1, &VertexBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
-	loadObj("models/sphere.obj", sphereModel.vertices, sphereModel.normals, sphereModel.UVs);
-	int size = sphereModel.vertices.size() * sizeof(glm::vec3) + sphereModel.normals.size() * sizeof(glm::vec3) + sphereModel.UVs.size() * sizeof(glm::vec2);
-	glBufferData(GL_ARRAY_BUFFER, size, NULL,GL_STATIC_DRAW);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sphereModel.vertices.size() * sizeof(glm::vec3), &sphereModel.vertices[0]);
-	glBufferSubData(GL_ARRAY_BUFFER, sphereModel.vertices.size() * sizeof(glm::vec3), sphereModel.normals.size() * sizeof(glm::vec3), &sphereModel.normals[0]);
-	glBufferSubData(GL_ARRAY_BUFFER, sphereModel.vertices.size() * sizeof(glm::vec3) + sphereModel.normals.size() * sizeof(glm::vec3), sphereModel.UVs.size() * sizeof(glm::vec2), &sphereModel.UVs[0]);
-	GLuint spherePos = glGetAttribLocation(programTexture, "vertexPosition");
-	glEnableVertexAttribArray(spherePos);
-	glVertexAttribPointer(spherePos, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
-
-	GLuint sphereNor = glGetAttribLocation(programTexture, "vertexNormal");
-	glEnableVertexAttribArray(sphereNor);
-	glVertexAttribPointer(sphereNor, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sphereModel.vertices.size() * sizeof(glm::vec3)));
-	
-	GLuint sphereUV = glGetAttribLocation(programTexture, "vertexTexCoord");
-	glEnableVertexAttribArray(sphereUV);
-	glVertexAttribPointer(sphereUV, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sphereModel.vertices.size() * sizeof(glm::vec3) + sphereModel.normals.size() * sizeof(glm::vec3)));
-	glBindVertexArray(0);*/
-
-	//textureEarth = Core::LoadTexture("textures/earth.png");
-	//textureRoughness = Core::LoadTexture("textures/rustediron2_roughness.png");
-	//textureMetallic = Core::LoadTexture("textures/rustediron2_metallic.png");
-	//textureAlbedo = Core::LoadTexture("textures/rustediron2_basecolor.png");
-	
 }
 
 void shutdown()	
 {
-	shaderLoader.DeleteProgram(programColor);
 	shaderLoader.DeleteProgram(programTexture);
 }
 
